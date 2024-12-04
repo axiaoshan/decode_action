b5116df68db54f8169d8023733ff0aa16d826791
action <action@github.com>
2024-12-04 14:46:39 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,4 +1,4 @@
-//Wed Oct 30 2024 05:35:05 GMT+0000 (Coordinated Universal Time)
+//Wed Dec 04 2024 14:46:39 GMT+0000 (Coordinated Universal Time)
 //Base:https://github.com/echo094/decode-js
 //Modify:https://github.com/smallfawn/decode_action
 const $ = new Env("玩一玩_下单打卡全额返");
