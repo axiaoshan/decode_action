6d2dd61d3c7554317959c45b14f391ab8b0df15c
action <action@github.com>
2025-11-16 12:30:33 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,4 +1,4 @@
-//Thu Oct 23 2025 10:48:19 GMT+0000 (Coordinated Universal Time)
+//Sun Nov 16 2025 12:30:33 GMT+0000 (Coordinated Universal Time)
 //Base:https://github.com/echo094/decode-js
 //Modify:https://github.com/smallfawn/decode_action
 NAME = "徐汇通";
